1e5256d3960d369864653abd6b39732ca0d968e1
Matthew Martin <matthewdeanmartin@gmail.com>
2024-02-10 18:54:40 -0500

    [skip ci] benchmark code

--- benchmark/main.py
+++ benchmark/main.py
@@ -2,10 +2,22 @@ import timeit
 import pickle
 import json
 import yaml
+from asteval import Interpreter
 import hissbytenotation as hbn
 import test.generate as generator
 
 data = generator.generate_test_data(no_bytes=True, no_sets=True, no_elipsis=True)
+
+aeval = Interpreter(minimal=True)
+
+def benchmark_just_repr():
+    # load
+    repr_string = repr(data)
+
+def benchmark_asteval():
+    # load
+    repr_string = repr(data)
+    _result = aeval(repr_string)
 
 
 def benchmark_hbn_no_validate_exec():
@@ -51,23 +63,29 @@ def benchmark_yaml():
 
     yaml.load(serialized, Loader=yaml.FullLoader)
 
+count=1000
 
-pickle_time = timeit.timeit(benchmark_pickle, number=10000)
-json_time = timeit.timeit(benchmark_json, number=10000)
+pickle_time = timeit.timeit(benchmark_pickle, number=count)
+json_time = timeit.timeit(benchmark_json, number=count)
 # can't represent a lot?
-# yaml_time = timeit.timeit(benchmark_yaml, number=10000)
-hbn_time_no_validate = timeit.timeit(benchmark_hbn_no_validate, number=10000)
-hbn_time_validate = timeit.timeit(benchmark_hbn_validate, number=10000)
-hbn_time_by_eval = timeit.timeit(benchmark_hbn_no_validate_by_eval, number=10000)
-hbn_time_exec = timeit.timeit(benchmark_hbn_no_validate_exec, number=10000)
-hbn_time_by_import = timeit.timeit(benchmark_hbn_no_validate_by_import, number=10000)
+# yaml_time = timeit.timeit(benchmark_yaml, number=count)
+hbn_time_no_validate = timeit.timeit(benchmark_hbn_no_validate, number=count)
+hbn_time_validate = timeit.timeit(benchmark_hbn_validate, number=count)
+hbn_time_by_eval = timeit.timeit(benchmark_hbn_no_validate_by_eval, number=count)
+hbn_time_exec = timeit.timeit(benchmark_hbn_no_validate_exec, number=count)
+hbn_time_by_import = timeit.timeit(benchmark_hbn_no_validate_by_import, number=count)
 
-print("## Serialization and deserialization times for 10,000 dumps/loads")
-print(f"Pickle:  {pickle_time:.6f} seconds")
-print(f"JSON: {json_time:.6f} seconds")
-# print(f"YAML serialization and deserialization time: {yaml_time:.6f} seconds")
-print(f"HBN (no validation): {hbn_time_no_validate:.6f} seconds")
-print(f"HBN (validation): {hbn_time_validate:.6f} seconds")
-print(f"HBN (eval): {hbn_time_by_eval:.6f} seconds")
-print(f"HBN (exec): {hbn_time_exec:.6f} seconds")
-print(f"HBN (by import): {hbn_time_by_import:.6f} seconds")
+repr_time = timeit.timeit(benchmark_just_repr, number=count)
+asteval_time = timeit.timeit(benchmark_asteval, number=count)
+
+print(f"## Serialization and deserialization times for {count} dumps/loads")
+print(f"Oneway repr {repr_time:.2f} seconds")
+print(f"Pickle:  {pickle_time:.2f} seconds")
+print(f"JSON: {json_time:.2f} seconds")
+print(f"asteval (repr + Interpreter()):  {asteval_time:.2f} seconds")
+# print(f"YAML serialization and deserialization time: {yaml_time:.2f} seconds")
+print(f"HBN (repr + ast_eval, no validation): {hbn_time_no_validate:.2f} seconds")
+print(f"HBN (repr + ast_eval + validation): {hbn_time_validate:.2f} seconds")
+print(f"HBN (repr + eval): {hbn_time_by_eval:.2f} seconds")
+print(f"HBN (repr + exec): {hbn_time_exec:.2f} seconds")
+print(f"HBN (repr + by import): {hbn_time_by_import:.2f} seconds")
